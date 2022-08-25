371d54a5bc48f3f85187bce532df0524b1723af6
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-08-24 20:02:15 -0400

    corrige espaçamentos

--- src/challenges.js
+++ src/challenges.js
@@ -1,19 +1,17 @@
 // Desafio 1
 function compareTrue(valor1, valor2 ) {
-
   return valor1 && valor2 === true
-}
+};
 
 // Desafio 2
 function calcArea(base, heigth) {
   return (base * heigth) / 2;
-}
+};
 
 // Desafio 3
 function splitSentence(string) {
-
     return string.split([' '])
-}
+};
 
 // Desafio 4
 function concatName() {
