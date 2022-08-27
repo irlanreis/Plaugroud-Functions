dda7c687a00dfbbb22c6cf7048c23648916896d8
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-08-27 02:19:49 -0400

    corrige ponto e virgula

--- src/challenges.js
+++ src/challenges.js
@@ -29,20 +29,23 @@ function footballPoints(wins , ties) {
 }
 // Desafio 6
 function highestCount(arr) {
-  let numeroMaior = 0;
-  let indice = 0;
-
-  for (let index = 0; index < arr.length; index += 1 ) {
-    if (arr[index] > numeroMaior) {
-      numeroMaior = arr[index];
-    }
+  let big = 0;
+    
+  let idx = 0;
+  
+  for (let i = 0; i < arr.length; i += 1) {
+ 
+      if ( arr[i] > 0 || arr[i] < 0 ) {
+    
+          big = arr[i];
   }
-  for (let index2 = 0; index2 < arr.length; index2 += 1) {
-    if (arr[index2] === numeroMaior) {
-      indice += 1;
-    }
-  }
-  return indice;
+}
+      for ( let index in arr ) {
+          if (arr[index] === big) {
+              idx += 1;
+          }
+      }
+  return idx;
 }
 
 // Desafio 7
